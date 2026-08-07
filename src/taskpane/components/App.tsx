956e93eb534f549001e8100ecdf7d61866a0787c
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-08-08 00:40:53 +0500

    refactor core app shell styling properties for sleek look

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -25,7 +25,6 @@ export default function App() {
         }}
       >
         <Stack spacing={1.5}>
-          {/* Main Component Layer */}
           <ContactsPage />
         </Stack>
       </Box>

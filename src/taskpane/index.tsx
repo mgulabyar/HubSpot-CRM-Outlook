a1fee439736ef0695e7a7514975d63ca310dca03
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-08-10 00:16:04 +0500

    feat: add HubSpot owner dropdown

--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -10,7 +10,6 @@ const title = "Contoso Task Pane Add-in";
 const rootElement: HTMLElement | null = document.getElementById("container");
 const root = rootElement ? createRoot(rootElement) : undefined;
 
-/* Render application after Office initializes */
 Office.onReady(() => {
   root?.render(
     <FluentProvider theme={webLightTheme}>

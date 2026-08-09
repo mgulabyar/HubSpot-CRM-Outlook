dffd223a71d05b8631e3207273e14c83a45eea92
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-08-10 00:16:47 +0500

    feat: add task edit modal

--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App";
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 
-/* global document, Office, module, require,  */
+/* global document,odule, require,  */
 
 const title = "Contoso Task Pane Add-in";
 
